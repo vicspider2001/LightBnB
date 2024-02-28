2675a7f3d4aa037e982260d43bb50b96e3f93fe2
Victor Anogwih <anogwihvictor@gmail.com>
2024-02-27 20:51:55 -0500

    reservations_function_uodated

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -82,7 +82,23 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows; // Return the rowS
+    
+  })
+  .catch((err) => {
+    console.log(err.message); // logs the error
+  });
 };
 
 /// Properties
